0f4b226b97ae7440d9c9d0482974a7fb9bc9f52a
Atharv <atharvgupta098@gmail.com>
2025-09-23 00:45:49 +0530

    Integrated News-API and dummy data from API

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,16 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news Component.
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>
+        <h3>News-Monkey Top Headlines:</h3>
+        <div className="sub-container grid grid-cols-3 gap-4">
+            <NewsItem/>
+            <NewsItem/>
+            <NewsItem/>
+            <NewsItem/>
+            <NewsItem/>
+            <NewsItem/>
+        </div>
       </div>
 
     )
